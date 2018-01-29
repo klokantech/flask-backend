b241f43f840626bf0001c8f4a67299096ecf54eb
Václav Klusák <vaclav.klusak@klokantech.com>
2018-01-29 10:39:51 +0100

    Release 1.6.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Backend',
-    version='1.5',
+    version='1.6',
     description='Asynchronous backend tasks for Flask',
     py_modules=['flask_backend'],
     install_requires=[
